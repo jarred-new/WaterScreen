fcd0b3cd58c507fb0062001520cb59431cbee1d2
JarredIzKool <158783902+jarred-new@users.noreply.github.com>
2026-05-28 15:19:11 +0800

    Add files via upload

---
+++ TextDisplay/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TextDisplay
+{
+    class Program
+    {
+	
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string text = args.Length > 0 ? args[0] : "You need to enter your text!!!";
+            Console.Title = text;
+
+            string fontName = args.Length > 1 ? args[1] : "Segoe UI";
+            float fontSize = args.Length > 2 ? float.Parse(args[2]) : 32f;
+
+            Color textColor = Color.White;
+
+            try
+            {
+                if (args.Length > 3)
+                {
+                    textColor = ColorTranslator.FromHtml(args[3]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Color not found or invalid");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Color transparencyKey = Color.Lime;
+
+            try
+            {
+                if (args.Length > 4)
+                {
+                    transparencyKey = ColorTranslator.FromHtml(args[4]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Color not found or invalid");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            int displayIndex = 0;
+
+            try
+            {
+                if (args.Length > 5)
+                {
+                    displayIndex = int.Parse(args[5]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Display not found");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Screen[] screens = Screen.AllScreens;
+
+            if (displayIndex < 0 || displayIndex >= screens.Length)
+            {
+                Console.WriteLine("Display " + displayIndex +
+                    " not found\nYour Connected displays: " +
+                    screens.Length);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            OverlayPosition position = OverlayPosition.TopRight;
+
+            if (args.Length > 6)
+            {
+                try
+                {
+                    position = (OverlayPosition)
+                        Enum.Parse(
+                            typeof(OverlayPosition),
+                            args[6],
+                            true);
+                }
+                catch
+                {
+                    Console.WriteLine(
+                        "Invalid position.\n" +
+                        "Valid values:\n" +
+                        "TopLeft, TopCenter, TopRight,\n" +
+                        "CenterLeft, Center, CenterRight,\n" +
+                        "BottomLeft, BottomCenter, BottomRight");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                    return;
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new TextDisplayForm(
+                text, fontName, fontSize, textColor, transparencyKey, screens[displayIndex], position));
+        }
+    }
+}
---
+++ TextDisplay/TextDisplayForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TextDisplay
+{
+    class TextDisplayForm : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int WS_EX_TRANSPARENT = 0x20;
+                const int WS_EX_LAYERED = 0x80000;
+
+                CreateParams cp =
+                    base.CreateParams;
+
+                cp.ExStyle |=
+                    WS_EX_TRANSPARENT |
+                    WS_EX_LAYERED;
+
+                return cp;
+            }
+        }
+        public TextDisplayForm(string text, string fontName, float fontSize,
+            Color color, Color transparencyColor, Screen screen, OverlayPosition position)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+
+            this.BackColor = transparencyColor;
+            this.TransparencyKey = transparencyColor;
+            //this.BackColor = Color.Transparent;
+
+            this.StartPosition = FormStartPosition.Manual;
+
+            this.Left = screen.Bounds.Left;
+            this.Top = screen.Bounds.Top;
+            this.Width = screen.Bounds.Width;
+            this.Height = screen.Bounds.Height;
+
+            Label lbl = new Label();
+            lbl.AutoSize = true;
+            lbl.Text = text;
+            lbl.ForeColor = color;
+            lbl.BackColor = Color.Transparent;
+            lbl.Font = new Font(fontName, fontSize);
+
+            this.Controls.Add(lbl);
+
+            this.Load += delegate
+            {
+                this.PositionLabel(lbl, position);
+            };
+        }
+
+        private void PositionLabel(Label lbl, OverlayPosition pos)
+        {
+            const int margin = 20;
+
+            int x = margin;
+            int y = margin;
+
+            switch (pos)
+            {
+                case OverlayPosition.TopLeft:
+                    x = margin;
+                    y = margin;
+                    break;
+
+                case OverlayPosition.TopCenter:
+                    x = (ClientSize.Width - lbl.Width) / 2;
+                    y = margin;
+                    break;
+
+                case OverlayPosition.TopRight:
+                    x = ClientSize.Width - lbl.Width - margin;
+                    y = margin;
+                    break;
+
+                case OverlayPosition.CenterLeft:
+                    x = margin;
+                    y = (ClientSize.Height - lbl.Height) / 2;
+                    break;
+
+                case OverlayPosition.Center:
+                    x = (ClientSize.Width - lbl.Width) / 2;
+                    y = (ClientSize.Height - lbl.Height) / 2;
+                    break;
+
+                case OverlayPosition.CenterRight:
+                    x = ClientSize.Width - lbl.Width - margin;
+                    y = (ClientSize.Height - lbl.Height) / 2;
+                    break;
+
+                case OverlayPosition.BottomLeft:
+                    x = margin;
+                    y = ClientSize.Height - lbl.Height - margin;
+                    break;
+
+                case OverlayPosition.BottomCenter:
+                    x = (ClientSize.Width - lbl.Width) / 2;
+                    y = ClientSize.Height - lbl.Height - margin;
+                    break;
+
+                case OverlayPosition.BottomRight:
+                    x = ClientSize.Width - lbl.Width - margin;
+                    y = ClientSize.Height - lbl.Height - margin;
+                    break;
+            }
+
+            lbl.Location = new Point(x, y);
+        }
+    }
+}
